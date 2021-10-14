385ab1c6d40e4692264b0b957c2c1466d8c4c491
Andy Marke <andy.marke@digital.justice.gov.uk>
2021-10-14 15:54:32 +0100

    DT-2738 Fixed immutable issue in test using reflection

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/jpa/transformer/TransformersKtTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/jpa/transformer/TransformersKtTest.kt
@@ -5,16 +5,11 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.PrisonCaseload
+import uk.gov.justice.digital.hmpps.nomisuserrolesapi.helper.userPersonalDetails
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.Caseload
-import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.Role
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.Staff
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.UsageType
-import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.UserCaseload
-import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.UserCaseloadPk
-import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.UserCaseloadRole
-import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.UserCaseloadRoleIdentity
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.UserPersonDetail
-import java.time.LocalDate
 
 internal class TransformersKtTest {
 
@@ -39,6 +34,7 @@ internal class TransformersKtTest {
       assertThat(data.staffId).isEqualTo(99)
       assertThat(data.activeCaseload).isNotNull
     }
+
     @Test
     fun `will copy names, caseload and capitalize`() {
       val entity = UserPersonDetail(
@@ -70,32 +66,12 @@ internal class TransformersKtTest {
 
     @Test
     internal fun `will copy the count of all the DPS roles`() {
-      val entity = UserPersonDetail(
-        username = "raj.maki",
-        staff = Staff(staffId = 99, firstName = "RAJ", lastName = "MAKI", status = "ACTIVE"),
-        type = UsageType.GENERAL,
-        activeCaseLoad = Caseload("WWI", "WANDSWORTH (HMP)"),
-      ).apply {
-        this.dpsRoles.add(userCaseloadRole(1))
-        this.dpsRoles.add(userCaseloadRole(2))
-        this.dpsRoles.add(userCaseloadRole(3))
-      }
+      val entity =
+        userPersonalDetails(roles = listOf("APPROVE_CATEGORISATION", "CREATE_CATEGORISATION", "GLOBAL_SEARCH"))
 
       val data = entity.toUserSummary()
 
       assertThat(data.dpsRoleCount).isEqualTo(3)
     }
   }
-
-  private fun UserPersonDetail.userCaseloadRole(roledId: Long) = UserCaseloadRole(
-    id = UserCaseloadRoleIdentity(roledId, "raj.maki", "WANDSWORTH (HMP)"),
-    role = Role(code = "OMIC_ROLE", name = "OMIC Fole"),
-    UserCaseload(
-      id = UserCaseloadPk(caseloadId = "WWI", username = "raj.maki"),
-      caseload = Caseload("WWI", "WANDSWORTH (HMP)"),
-      user = this,
-      roles = listOf(),
-      startDate = LocalDate.now(),
-    )
-  )
 }
