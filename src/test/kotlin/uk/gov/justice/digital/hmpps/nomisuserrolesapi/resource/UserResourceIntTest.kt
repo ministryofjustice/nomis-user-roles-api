081844a5a1e474c69919b1005ce758eae0f66998
Michael Willis <michael.willis@digital.justice.gov.uk>
2021-10-14 14:33:17 +0100

    DT-2740 Lock Unlock User accounts & Change Password (#29)
    * DT-2740 Lock Unlock User accounts & Change Password

--- src/test/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/resource/UserResourceIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/resource/UserResourceIntTest.kt
@@ -886,4 +886,136 @@ class UserResourceIntTest : IntegrationTestBase() {
         .expectStatus().isNotFound
     }
   }
+
+  @Nested
+  @DisplayName("PUT /users/{username}/lock-user")
+  inner class LockUnlockUsers {
+
+    @BeforeEach
+    internal fun createUsers() {
+      webTestClient.post().uri("/users/general-account")
+        .headers(setAuthorisation(roles = listOf("ROLE_CREATE_USER")))
+        .body(
+          BodyInserters.fromValue(
+            CreateGeneralUserRequest(
+              username = "LOCKING_USER1",
+              firstName = "Locking",
+              lastName = "User",
+              email = "test@test.com",
+              defaultCaseloadId = "PVI"
+            )
+          )
+        )
+        .exchange()
+        .expectStatus().isCreated
+    }
+
+    @AfterEach
+    internal fun deleteUsers() {
+      webTestClient.delete().uri("/users/LOCKING_USER1")
+        .headers(setAuthorisation(roles = listOf("ROLE_CREATE_USER")))
+        .exchange()
+        .expectStatus().isOk
+    }
+
+    @Test
+    fun `can't lock a db user that doesn't exist`() {
+      webTestClient.put().uri("/users/LOCKING_USER2/lock-user")
+        .headers(setAuthorisation(roles = listOf("ROLE_MANAGE_NOMIS_USER_ACCOUNT")))
+        .exchange()
+        .expectStatus().isNotFound
+    }
+
+    @Test
+    fun `can't lock a db user without correct role`() {
+      webTestClient.put().uri("/users/LOCKING_USER1/lock-user")
+        .headers(setAuthorisation(roles = listOf("ROLE_DUMMY")))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `can lock a db user that does exist`() {
+      webTestClient.put().uri("/users/LOCKING_USER1/lock-user")
+        .headers(setAuthorisation(roles = listOf("ROLE_MANAGE_NOMIS_USER_ACCOUNT")))
+        .exchange()
+        .expectStatus().isOk
+
+      webTestClient.get().uri("/users/LOCKING_USER1")
+        .headers(setAuthorisation(roles = listOf("ROLE_MAINTAIN_ACCESS_ROLES_ADMIN")))
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("accountStatus").isEqualTo("LOCKED")
+    }
+
+    @Test
+    fun `can't unlock a db user that doesn't exist`() {
+      webTestClient.put().uri("/users/LOCKING_USER2/unlock-user")
+        .headers(setAuthorisation(roles = listOf("ROLE_MANAGE_NOMIS_USER_ACCOUNT")))
+        .exchange()
+        .expectStatus().isNotFound
+    }
+
+    @Test
+    fun `can't unlock a db user without correct role`() {
+      webTestClient.put().uri("/users/LOCKING_USER1/unlock-user")
+        .headers(setAuthorisation(roles = listOf("ROLE_DUMMY")))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `can unlock a db user that does exist`() {
+      webTestClient.put().uri("/users/LOCKING_USER1/unlock-user")
+        .headers(setAuthorisation(roles = listOf("ROLE_MANAGE_NOMIS_USER_ACCOUNT")))
+        .exchange()
+        .expectStatus().isOk
+
+      webTestClient.get().uri("/users/LOCKING_USER1")
+        .headers(setAuthorisation(roles = listOf("ROLE_MAINTAIN_ACCESS_ROLES_ADMIN")))
+        .exchange()
+        .expectStatus().isOk
+        .expectBody()
+        .jsonPath("accountStatus").isEqualTo("OPEN")
+    }
+
+    @Test
+    fun `can't change password of a db user that doesn't exist`() {
+      webTestClient.put().uri("/users/LOCKING_USER2/change-password")
+        .headers(setAuthorisation(roles = listOf("ROLE_MANAGE_NOMIS_USER_ACCOUNT")))
+        .body(BodyInserters.fromValue("He110W0R1D5555"))
+        .exchange()
+        .expectStatus().isNotFound
+    }
+
+    @Test
+    fun `can't change password of a db user without correct role`() {
+      webTestClient.put().uri("/users/LOCKING_USER1/change-password")
+        .headers(setAuthorisation(roles = listOf("ROLE_DUMMY")))
+        .body(BodyInserters.fromValue("He110W0R1D5555"))
+        .exchange()
+        .expectStatus().isForbidden
+    }
+
+    @Test
+    fun `can change password of a  db user that does exist`() {
+      webTestClient.put().uri("/users/LOCKING_USER1/change-password")
+        .headers(setAuthorisation(roles = listOf("ROLE_MANAGE_NOMIS_USER_ACCOUNT")))
+        .body(BodyInserters.fromValue("He110W0R1D5555"))
+        .exchange()
+        .expectStatus().isOk
+    }
+
+    @Test
+    fun `can't change password of a db user for an invalid password`() {
+      webTestClient.put().uri("/users/LOCKING_USER1/change-password")
+        .headers(setAuthorisation(roles = listOf("ROLE_MANAGE_NOMIS_USER_ACCOUNT")))
+        .body(BodyInserters.fromValue("HELLO^%$"))
+        .exchange()
+        .expectStatus().is4xxClientError
+        .expectBody()
+        .jsonPath("userMessage").isEqualTo("Validation failure: changePassword.password: Password must consist of alphanumeric characters only and a minimum of 14 chars, and max 30 chars")
+    }
+  }
 }
