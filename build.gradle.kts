36c63d11413b871fba18f69c7a9290c92008bb27
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2022-01-12 07:49:29 +0000

    DT-3023: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.1 (#84)
    includes springboot 2.6.2 and kotlin 1.6.10 and jvmTarget to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
-  kotlin("plugin.spring") version "1.6.0"
-  kotlin("plugin.jpa") version "1.6.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.1"
+  kotlin("plugin.spring") version "1.6.10"
+  kotlin("plugin.jpa") version "1.6.10"
   idea
 }
 
@@ -20,24 +20,24 @@ dependencies {
   implementation("org.apache.commons:commons-text:1.9")
   implementation("commons-codec:commons-codec:1.15")
 
-  implementation("org.flywaydb:flyway-core:8.2.2")
+  implementation("org.flywaydb:flyway-core:8.4.1")
   implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
 
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.1")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.1")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.1")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.4")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.4")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
 
-  implementation("com.zaxxer:HikariCP:5.0.0")
-  runtimeOnly("com.h2database:h2:2.0.202")
-  runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.12.0.0")
+  implementation("com.zaxxer:HikariCP:5.0.1")
+  runtimeOnly("com.h2database:h2:2.0.206")
+  runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.13.0.0.1")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
@@ -46,17 +46,16 @@ dependencies {
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.mockito:mockito-inline:4.2.0")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "16"
+      jvmTarget = "17"
     }
   }
 }
