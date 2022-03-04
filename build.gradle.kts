07295d6b12cef0b66763a068154648b068d2281b
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-03-04 08:53:46 +0000

    HAAR-183: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.5-beta and include CircleCi Jira Integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.3-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.5-beta"
   kotlin("plugin.spring") version "1.6.10"
   kotlin("plugin.jpa") version "1.6.10"
   idea
@@ -20,7 +20,7 @@ dependencies {
   implementation("org.apache.commons:commons-text:1.9")
   implementation("commons-codec:commons-codec:1.15")
 
-  implementation("org.flywaydb:flyway-core:8.4.3")
+  implementation("org.flywaydb:flyway-core:8.5.0")
   implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
@@ -28,9 +28,9 @@ dependencies {
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.5")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.5")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.5")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
