4558f80e91e8f7db2e76896f2f4b7ed2fb24bbf1
Michael Willis <michael.willis@digital.justice.gov.uk>
2022-04-01 16:05:46 +0100

    HAAR-301 Reduce timesout to HMPPS Auth (#113)
    * HAAR-301 reduce timeouts to Auth
    
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>
    
    * HAAR-301 reduce timeouts to Auth
    
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/config/WebClientConfiguration.kt
@@ -1,24 +1,51 @@
 package uk.gov.justice.digital.hmpps.nomisuserrolesapi.config
 
+import io.netty.channel.ChannelOption
+import io.netty.handler.timeout.ReadTimeoutHandler
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
+import org.springframework.http.client.reactive.ClientHttpConnector
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.Connection
+import reactor.netty.http.client.HttpClient
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.utils.UserContext
+import java.time.Duration
 
 @Configuration
-class WebClientConfiguration(@Value("\${api.base.url.oauth}") val authbaseUri: String) {
+class WebClientConfiguration(
+  @Value("\${api.base.url.oauth}") val authbaseUri: String,
+  @Value("\${api.base.timeout.oauth:2s}") val timeout: Duration
+) {
 
   @Bean
   fun webClient(builder: WebClient.Builder): WebClient {
+
     return builder
       .baseUrl(authbaseUri)
+      .clientConnector(getClientConnectorWithTimeouts(timeout, timeout))
       .filter(addAuthHeaderFilterFunction())
       .build()
+  }
+
+  private fun getClientConnectorWithTimeouts(
+    connectTimeout: Duration,
+    readTimeout: Duration
+  ): ClientHttpConnector {
+    val httpClient = HttpClient.create()
+    return ReactorClientHttpConnector(
+      httpClient
+        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeout.toMillis().toInt())
+        .doOnConnected { connection: Connection ->
+          connection
+            .addHandlerLast(ReadTimeoutHandler(readTimeout.toSeconds().toInt()))
+        }
+    )
   }
 
   private fun addAuthHeaderFilterFunction(): ExchangeFilterFunction {
