bd63182e3cb2b24cb6931241998d80eae2b770a2
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2022-04-20 15:21:21 +0100

    HAAR-350: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.1.5-beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4-beta"
-  kotlin("plugin.spring") version "1.6.10"
-  kotlin("plugin.jpa") version "1.6.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.5-beta"
+  kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.6.21"
   idea
 }
 
@@ -20,23 +20,23 @@ dependencies {
   implementation("org.apache.commons:commons-text:1.9")
   implementation("commons-codec:commons-codec:1.15")
 
-  implementation("org.flywaydb:flyway-core:8.5.4")
-  implementation("com.vladmihalcea:hibernate-types-52:2.14.1")
+  implementation("org.flywaydb:flyway-core:8.5.8")
+  implementation("com.vladmihalcea:hibernate-types-52:2.16.1")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
 
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.7")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
   implementation("com.pauldijou:jwt-core_2.11:5.0.0")
 
   implementation("com.zaxxer:HikariCP:5.0.1")
-  runtimeOnly("com.h2database:h2:2.1.210")
+  runtimeOnly("com.h2database:h2:2.1.212")
   runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.14.0.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -45,7 +45,7 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.mockito:mockito-inline:4.4.0")
+  testImplementation("org.mockito:mockito-inline:4.5.0")
 }
 
 java {
