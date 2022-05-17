98cf344cd166dde1a7c4a788450b713629673298
mjwillis <michael.willis@digital.justice.gov.uk>
2022-05-17 12:30:59 +0100

    HAAR-438 KLint
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/jpa/repository/UserPersonDetailRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/jpa/repository/UserPersonDetailRepository.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa.repository
 
-import org.springframework.data.domain.Page
-import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Modifying
