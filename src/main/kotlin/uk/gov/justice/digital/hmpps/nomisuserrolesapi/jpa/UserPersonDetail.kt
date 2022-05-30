813928efa89abb336cc5d0e7cb2453b83f926c34
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2022-05-30 11:23:50 +0100

    HAAR-470: Download endpoint:- Add report based (for CSV) of list of users based on same criteria as used by BETA user search

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/jpa/UserPersonDetail.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/jpa/UserPersonDetail.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.nomisuserrolesapi.jpa
 
 import org.hibernate.Hibernate
+import org.hibernate.annotations.BatchSize
 import org.hibernate.annotations.Where
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.PrisonCaseload
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.UserSummary
@@ -19,6 +20,9 @@ import javax.persistence.FetchType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
+import javax.persistence.NamedAttributeNode
+import javax.persistence.NamedEntityGraph
+import javax.persistence.NamedSubgraph
 import javax.persistence.OneToMany
 import javax.persistence.PrimaryKeyJoinColumn
 import javax.persistence.SecondaryTable
@@ -28,34 +32,71 @@ import javax.persistence.Table
 @Entity
 @Table(name = "STAFF_USER_ACCOUNTS")
 @SecondaryTable(name = "DBA_USERS", pkJoinColumns = [PrimaryKeyJoinColumn(name = "USERNAME")])
+@BatchSize(size = 100)
+
+@NamedEntityGraph(
+  name = "user-person-detail-download-graph",
+  attributeNodes = [
+    NamedAttributeNode("username"), NamedAttributeNode(
+      value = "staff",
+      subgraph = "staff-subgraph"
+    )
+  ],
+  subgraphs = [
+    NamedSubgraph(
+      name = "staff-subgraph",
+      attributeNodes = [
+        NamedAttributeNode(value = "staffId"), NamedAttributeNode(value = "firstName"), NamedAttributeNode(
+          value = "lastName"
+        ), NamedAttributeNode(value = "status"), NamedAttributeNode(
+          value = "emails",
+          subgraph = "emails-subgraph"
+        )
+      ]
+    ),
+    NamedSubgraph(
+      name = "emails-subgraph",
+      attributeNodes = [
+        NamedAttributeNode(value = "userType"),
+        NamedAttributeNode(value = "type"), NamedAttributeNode(value = "email")
+      ]
+    )
+  ]
+)
 data class UserPersonDetail(
   @Id
   @Column(name = "USERNAME", nullable = false)
   val username: String,
 
-  @ManyToOne(cascade = [CascadeType.ALL])
+  @ManyToOne(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
   @JoinColumn(name = "STAFF_ID")
   val staff: Staff,
 
   @OneToMany(fetch = FetchType.LAZY)
   @JoinColumn(name = "USERNAME", updatable = false, insertable = false, nullable = false)
   @Where(clause = "CASELOAD_ID = '$DPS_CASELOAD'")
+  @BatchSize(size = 100)
   val dpsRoles: List<UserCaseloadRole> = listOf(),
 
   @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL], mappedBy = "user", orphanRemoval = true)
+  @BatchSize(size = 100)
   val caseloads: MutableList<UserCaseload> = mutableListOf(),
 
   @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
+  @BatchSize(size = 100)
   val activeAndInactiveMemberOfUserGroups: MutableList<UserGroupMember> = mutableListOf(),
 
   @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
   @Where(clause = "ACTIVE_FLAG = 'Y'")
+  @BatchSize(size = 100)
   val memberOfUserGroups: List<UserGroupMember> = listOf(),
 
   @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
+  @BatchSize(size = 100)
   val activeAndInactiveAdministratorOfUserGroups: MutableList<UserGroupAdministrator> = mutableListOf(),
 
   @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+  @BatchSize(size = 100)
   @Where(clause = "ACTIVE_FLAG = 'Y'")
   val administratorOfUserGroups: List<UserGroupAdministrator> = listOf(),
 
@@ -192,6 +233,20 @@ fun UserPersonDetail.toUserSummaryWithEmail() = UserSummaryWithEmail(
     )
   },
   dpsRoleCount = this.dpsRoles.size,
+  email = staff.primaryEmail()?.email,
+)
+
+fun UserPersonDetail.toDownloadUserSummaryWithEmail() = UserSummaryWithEmail(
+  username = username,
+  staffId = staff.staffId,
+  firstName = staff.firstName.capitalizeFully(),
+  lastName = staff.lastName.capitalizeFully(),
+  active = isActive(),
+  status = accountDetail?.status,
+  locked = accountDetail?.isLocked() ?: false,
+  expired = accountDetail?.isExpired() ?: false,
+  activeCaseload = null,
+  dpsRoleCount = 0,
   email = staff.primaryEmail()?.email,
 )
 
