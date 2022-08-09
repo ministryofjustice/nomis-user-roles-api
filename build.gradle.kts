a00c781499bc868af046f90b6b92a9b63458c821
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2022-08-09 16:44:47 +0100

    HAAR-697 upgrade dependencies (#167)

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
   implementation("commons-codec:commons-codec:1.15")
 
   implementation("org.flywaydb:flyway-core:8.5.13")
-  implementation("com.vladmihalcea:hibernate-types-52:2.16.2")
+  implementation("com.vladmihalcea:hibernate-types-52:2.17.3")
 
   implementation("javax.transaction:javax.transaction-api:1.3")
   implementation("javax.xml.bind:jaxb-api:2.3.1")
