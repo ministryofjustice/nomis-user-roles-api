b11a89614fcff5074e3ca10c9a6ee6eddf07806e
mjwillis <michael.willis@digital.justice.gov.uk>
2023-11-22 17:18:36 +0000

    HAAR-2090 Klint
    Signed-off-by: mjwillis <michael.willis@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/resource/UserRoleManagementResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/resource/UserRoleManagementResource.kt
@@ -11,7 +11,15 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.RoleAssignmentStats
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.RoleAssignmentsSpecification
