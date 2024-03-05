eb2b0f1269ef5a331af8b05628c212f382198268
thomasridd <twridd@gmail.com>
2024-03-05 16:58:02 +0000

    Add administratorOfUserGroups to standard UserDetail return type

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/data/UserGroup.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.nomisuserrolesapi.data
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import io.swagger.v3.oas.annotations.media.Schema
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Schema(description = "User Group Information")
+data class UserGroup(
+  @Schema(description = "Group code", example = "BXI", required = true) val code: String,
+  @Schema(description = "Group description", example = "Brixton (HMP)", required = true) val description: String,
+)
