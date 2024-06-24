5ecaaab204aa38f8e14b8c9893075814f31bbac8
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2024-06-24 09:15:00 +0100

    HAAR-2767:  ⬆️ Upgrade to latest spring boot 3.3.0 etc

--- scripts/add-roles-to-users/add-multiple-roles-to-users-loop.bash
+++ scripts/add-roles-to-users/add-multiple-roles-to-users-loop.bash
@@ -10,9 +10,25 @@ for user in $(cat $SCRIPTS_BASE_DIR/users); do
 
     echo "adding $role to $user"
 
-
+#LOCAL
 #     added=$(curl  -X 'POST' --location "http://localhost:8082/users/${user}/roles/${role}?caseloadId=NWEB" \
+#    --header 'Content-Type: application/json' \
+#    --header 'Authorization: Bearer <token>' \
+#    )
+
+#DEV
+#     added=$(curl  -X 'POST' --location "https://nomis-user-roles-api-dev.prison.service.justice.gov.uk/users/${user}/roles/${role}?caseloadId=NWEB" \
+#    --header 'Content-Type: application/json' \
+#    --header 'Authorization: Bearer <token>' \
+#    )
+
+#PREPROD
 #     added=$(curl  -X 'POST' --location "https://nomis-user-roles-api-preprod.prison.service.justice.gov.uk/users/${user}/roles/${role}?caseloadId=NWEB" \
+#    --header 'Content-Type: application/json' \
+#    --header 'Authorization: Bearer <token>' \
+#    )
+
+#PROD
     added=$(curl -X 'POST' --location "https://nomis-user-roles-api.prison.service.justice.gov.uk/users/${user}/roles/${role}?caseloadId=NWEB" \
     --header 'Content-Type: application/json' \
     --header 'Authorization: Bearer <token>' \
