287667ccf8e97ebcf2f11c54710eb6fbb2710984
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-11-06 11:43:56 +0000

    HAAR-2916 Cleaned up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  id("com.autonomousapps.dependency-analysis") version "2.4.2"
   kotlin("plugin.spring") version "2.0.21"
   kotlin("plugin.jpa") version "2.0.21"
   idea
@@ -16,38 +17,26 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("org.springframework.boot:spring-boot-starter-security")
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
   implementation("org.apache.commons:commons-lang3:3.17.0")
   implementation("org.apache.commons:commons-text:1.12.0")
   implementation("commons-codec:commons-codec:1.17.1")
-
-  implementation("org.flywaydb:flyway-core")
-  implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.8.3")
-  implementation("org.hibernate.orm:hibernate-community-dialects")
-
-  implementation("javax.transaction:javax.transaction-api:1.3")
-  implementation("javax.xml.bind:jaxb-api:2.3.1")
   implementation("com.google.guava:guava:33.3.1-jre")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
-
-  implementation("com.pauldijou:jwt-core_2.11:5.0.0")
-
-  implementation("com.zaxxer:HikariCP:6.0.0")
+  runtimeOnly("org.flywaydb:flyway-core")
+  runtimeOnly("io.hypersistence:hypersistence-utils-hibernate-60:3.8.3")
+  runtimeOnly("org.hibernate.orm:hibernate-community-dialects")
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.24.0.0")
 
   developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-  testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
   testImplementation("org.wiremock:wiremock-standalone:3.9.2")
-  testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
-  testImplementation("javax.xml.bind:jaxb-api:2.3.1")
+
+  testRuntimeOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
 }
 
 java {
