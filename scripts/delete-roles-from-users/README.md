2b988a0a3c9927518786e0bfd1ed219878f8a83d
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-01-23 15:40:07 +0000

    HAAR-3573: 🔨 script to bulk remove role(s) from list of users

---
+++ scripts/delete-roles-from-users/README.md
@@ -0,0 +1,38 @@
+# Script to remove single or multiple roles from list of users
+
+This script takes a list of usernames (users file) and a list of role codes (roles file), 
+The script loops through the users and calls the remove role endpoint to remove each role one 
+at a time.
+
+## Requirements
+
+* bash
+* curl
+
+## Adding users and roles
+
+Add the list of nomis usernames to the users file
+
+Add the list of role codes to the roles file
+
+(one per line)
+
+## Running the script
+
+It is normally a good idea to run the script against preprod before running it in prod
+
+You will need to obtain a bearer token from hmpps-auth and add it the bash command as a variable
+when the curl request called it has the necessary authorization to perform the action.
+
+you will also need to add the correct variable for the environment you are running the script against
+
+```./remove-multiple-roles-from-users-loop.bash <dev|preprod|prod> <bearer token>```
+
+if a user already has the role we are currently trying to add the response from the 
+curl request will be http 409 but the script will continue with the next role 
+
+## output
+
+The script will output to the terminal what it is currently doing.
+
+It will also output the response from each curl request into a file called removed.txt
---
+++ scripts/delete-roles-from-users/delete-multiple-roles-from-users-loop.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPTS_BASE_DIR=`pwd`
+
+ENV=${1}
+BEARER_TOKEN=${2}
+
+enforce_var_set() {
+  if [[ ! -v $1 || -z $(eval "echo \$$1") ]]; then
+    echo "$1 environment variable not set or is empty.  Please see README.md."
+    exit 1
+  fi
+}
+
+enforce_var_set ENV
+enforce_var_set BEARER_TOKEN
+
+if [ "$ENV" == "local" ]; then
+  BASE_URL="http://localhost:8082"
+elif [ "$ENV" == "dev" ]; then
+  BASE_URL="https://nomis-user-roles-api-dev.prison.service.justice.gov.uk"
+elif [ "$ENV" == "preprod" ]; then
+  BASE_URL="https://nomis-user-roles-api-preprod.prison.service.justice.gov.uk"
+elif [ "$ENV" == "prod" ]; then
+  BASE_URL="https://nomis-user-roles-api.prison.service.justice.gov.uk"
+else
+  echo "Invalid environment specified"
+  exit 1
+fi
+
+for user in $(cat $SCRIPTS_BASE_DIR/users); do
+  echo "updating user  - $user"
+
+    for role in $(cat $SCRIPTS_BASE_DIR/roles); do
+
+    echo "removing $role from $user"
+
+    removed=$(curl -X 'DELETE' --location "$BASE_URL/users/${user}/roles/${role}?caseloadId=NWEB" \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $BEARER_TOKEN" \
+    )
+    echo "removed"
+    echo "$role removed from $user"
+    echo "$user and $role = $removed" >> $SCRIPTS_BASE_DIR/removed.txt
+    done
+done
+
