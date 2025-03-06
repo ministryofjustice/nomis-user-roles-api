4f73c2ca0c258da05606d1f3dd48e176ba494f8b
Mike Bostock <17684266+mpbostock@users.noreply.github.com>
2025-03-06 11:11:24 +0000

    HAAR-3704: upgrade gradle-spring-boot to 7.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.3"
   kotlin("plugin.spring") version "2.1.10"
   kotlin("plugin.jpa") version "2.1.10"
   idea
@@ -16,14 +16,14 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
   implementation("org.apache.commons:commons-lang3:3.17.0")
   implementation("org.apache.commons:commons-text:1.13.0")
   implementation("commons-codec:commons-codec:1.18.0")
   implementation("com.google.guava:guava:33.4.0-jre")
 
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("io.hypersistence:hypersistence-utils-hibernate-60:3.9.1")
+  runtimeOnly("io.hypersistence:hypersistence-utils-hibernate-60:3.9.2")
   runtimeOnly("org.hibernate.orm:hibernate-community-dialects")
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("com.oracle.database.jdbc:ojdbc10:19.25.0.0")
@@ -32,7 +32,7 @@ dependencies {
 
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-  testImplementation("org.wiremock:wiremock-standalone:3.12.0")
+  testImplementation("org.wiremock:wiremock-standalone:3.12.1")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
 
   testRuntimeOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
