deb11af892c5bef4c9bc24c19de9624dfcb06861
AlTurner-MOJ <101654848+AlTurner-MOJ@users.noreply.github.com>
2025-04-30 11:03:35 +0100

    HAAR-3898: ⬆️ update to spring-boot-plugin-8.1.0 (#326)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
   kotlin("plugin.spring") version "2.1.20"
   kotlin("plugin.jpa") version "2.1.20"
   idea
@@ -18,9 +18,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
   implementation("org.apache.commons:commons-lang3:3.17.0")
-  implementation("org.apache.commons:commons-text:1.13.0")
+  implementation("org.apache.commons:commons-text:1.13.1")
   implementation("commons-codec:commons-codec:1.18.0")
-  implementation("com.google.guava:guava:33.4.6-jre")
+  implementation("com.google.guava:guava:33.4.8-jre")
 
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("io.hypersistence:hypersistence-utils-hibernate-60:3.9.4")
@@ -32,7 +32,7 @@ dependencies {
 
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.6")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
-  testImplementation("org.wiremock:wiremock-standalone:3.12.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.0")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
 
   testRuntimeOnly("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
