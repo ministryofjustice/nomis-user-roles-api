e5301d812c97ca8c7178fd0831fc8d8bfbcdb505
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-05-28 12:53:22 +0100

    MIA-365 update swagger docs

--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/data/JobClassificationRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/data/JobClassificationRequest.kt
@@ -1,19 +1,25 @@
 package uk.gov.justice.digital.hmpps.nomisuserrolesapi.data
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.PastOrPresent
 import java.time.LocalDate
 
 data class JobClassificationRequest(
+  @field:Schema(description = "The code of the position (based on nomis reference data)", example = "PO", required = true)
   @field:NotBlank("position must not be blank")
   val position: String,
+  @field:Schema(description = "The code of the schedule type (based on nomis reference data)", example = "FT", required = true)
   @field:NotBlank("schedule type must not be blank")
   val scheduleType: String,
+  @field:Schema(description = "The number of hours per week the staff member works", example = "40", required = true)
   @field:Min(value = 0, message = "hours per week must be a positive integer")
   val hoursPerWeek: Int,
+  @field:Schema(description = "The date the classification started (must be today or in the past)", required = true)
   @field:PastOrPresent("from date must be today or in the past")
   val fromDate: LocalDate,
+  @field:Schema(description = "The date the classification ended", required = false)
   @field:PastOrPresent("to date must be null, today or in the past")
   val toDate: LocalDate?,
 )
--- src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/resource/StaffMemberResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/nomisuserrolesapi/resource/StaffMemberResource.kt
@@ -1,5 +1,10 @@
 package uk.gov.justice.digital.hmpps.nomisuserrolesapi.resource
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import jakarta.validation.Valid
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PathVariable
@@ -7,6 +12,8 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.nomisuserrolesapi.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.CreateGeneralUserRequest
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.JobClassificationRequest
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.data.StaffJobClassification
 import uk.gov.justice.digital.hmpps.nomisuserrolesapi.service.StaffMemberService
@@ -14,6 +21,40 @@ import uk.gov.justice.digital.hmpps.nomisuserrolesapi.service.StaffMemberService
 @RestController
 @RequestMapping("/agency/{agencyId}/staff-members")
 class StaffMemberResource(private val staffMemberService: StaffMemberService) {
+  @Operation(
+    summary = "Create or update a job classification for a staff member",
+    description = "Creates a staff location role if one doesn't exist and expires duplicates if multiples exist. Requires role ROLE_NOMIS_MANAGE_USERS__STAFF_MEMBERS__RW",
+    security = [SecurityRequirement(name = "ROLE_NOMIS_MANAGE_USERS__STAFF_MEMBERS__RW")],
+    requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content = [
+        Content(
+          mediaType = "application/json",
+          schema = Schema(implementation = JobClassificationRequest::class),
+        ),
+      ],
+    ),
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "The job classification was created or updated successfully",
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Incorrect request to modify the staff job classification",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Incorrect permissions modify staff job classifications",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
   @PutMapping("/{staffId}/staff-role/{staffRole}")
   @PreAuthorize("hasRole('ROLE_NOMIS_MANAGE_USERS__STAFF_MEMBERS__RW')")
   fun setJobClassification(
