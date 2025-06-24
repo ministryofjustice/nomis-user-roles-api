7d81181dd7963f6f2631caf932a4994c8b47a8bc
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-06-24 14:37:28 +0100

    HAAR-4059: revert kotlin plugin update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
-  kotlin("plugin.spring") version "2.2.0"
+  kotlin("plugin.spring") version "2.1.21"
   kotlin("plugin.jpa") version "2.1.21"
   idea
 }
