b39cb20f885ad8e0681226bdf388fc34cf334bf8
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-07-22 11:03:50 +0100

    HAAR-4144: ⬆️ update to spring-boot-plugin-8.3.3 (#339)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
-  kotlin("plugin.spring") version "2.1.21"
-  kotlin("plugin.jpa") version "2.1.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.3"
+  kotlin("plugin.spring") version "2.2.0"
+  kotlin("plugin.jpa") version "2.2.0"
   idea
 }
 
@@ -17,7 +17,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
-  implementation("org.apache.commons:commons-lang3:3.17.0")
+  implementation("org.apache.commons:commons-lang3:3.18.0")
   implementation("org.apache.commons:commons-text:1.13.1")
   implementation("commons-codec:commons-codec:1.18.0")
   implementation("com.google.guava:guava:33.4.8-jre")
