5095a6d03a0e6b48266b8a38a84d751600ed26d3
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-01-27 11:54:11 +0000

    HAAR-4916: update to spring boot plugin 10.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.2"
   kotlin("plugin.spring") version "2.3.0"
   kotlin("plugin.jpa") version "2.3.0"
   idea
